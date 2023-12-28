1e57529b09a71a5f8e1ada3362be5b72414a7c59
mlamp99 <michael.lamp@avnet.com>
2023-12-28 10:04:18 -0600

    demo functions to set temperature and simulate temp rise and fall
    Added command:  set-temp

--- Projects/b_u585i_iot02a_ntz/Src/app/iotconnect_app.c
+++ Projects/b_u585i_iot02a_ntz/Src/app/iotconnect_app.c
@@ -14,6 +14,8 @@
 #include <stdlib.h>
 #include <assert.h>
 
+#include <math.h> // for sine function
+
 /* Kernel includes. */
 #include "FreeRTOS.h"
 #include "task.h"
@@ -51,18 +53,58 @@
 // Constants
 #define APP_VERSION 			"01.00.06"		// Version string in telemetry data
 #define MQTT_PUBLISH_PERIOD_MS 	( 3000 )		// Size of statically allocated buffers for holding topic names and payloads.
+#define LED_BLINK_PERIOD_MS 500  // Blink period in milliseconds
+
+
+// Temperature Simulation Parameters
+static bool simulateTemperature = false;
+static const float startTempF = 65.0f;  // Starting temperature in Fahrenheit
+static const float endTempF = 425.0f;   // Ending temperature in Fahrenheit
+static const int simDurationSec = 30;   // Duration of the temperature rise in seconds
+static TickType_t simStartTime;
+static float presetTempF = 0.0f;  // Preset temperature
+static bool targetTemperatureReached = false;
 
 // @brief	IOTConnect configuration defined by application
 static IotConnectAwsrtosConfig awsrtos_config;
 
 // Prototypes
+
 static BaseType_t init_sensors( void );
-static char* create_telemetry_json(IotclMessageHandle msg, BSP_MOTION_SENSOR_Axes_t accel_data,
-								BSP_MOTION_SENSOR_Axes_t gyro_data, BSP_MOTION_SENSOR_Axes_t mag_data);
+static char* create_telemetry_json(IotclMessageHandle msg, BSP_MOTION_SENSOR_Axes_t accel_data, BSP_MOTION_SENSOR_Axes_t gyro_data, BSP_MOTION_SENSOR_Axes_t mag_data, float simulatedTempF);
 static void on_command(IotclEventData data);
 static void command_status(IotclEventData data, bool status, const char *command_name, const char *message);
+static float simulateTemperatureRise(void);
 
 
+
+static float currentTempF = 65.0f; // Current temperature, initialize with start temperature
+
+static float simulateTemperatureRise() {
+    if (!simulateTemperature) {
+        return currentTempF; // Return the current temperature
+    }
+
+    TickType_t currentTime = xTaskGetTickCount();
+    float elapsedSec = (float)(currentTime - simStartTime) / pdMS_TO_TICKS(1000);
+    float rateOfChange = 5.0f; // Rate of temperature change per second
+    float tempDifference = presetTempF - currentTempF;
+    float tempChange = rateOfChange * elapsedSec;
+
+    if (fabs(tempDifference) < fabs(tempChange)) {
+        currentTempF = presetTempF;
+        targetTemperatureReached = true; // Target temperature reached
+    } else {
+        targetTemperatureReached = false; // Still transitioning
+        if (tempDifference > 0) {
+            currentTempF += tempChange;
+        } else {
+            currentTempF -= tempChange;
+        }
+    }
+
+    return currentTempF;
+}
 /* @brief	Main IoT-Connect application task
  *
  * @param	pvParameters, argument passed by xTaskCreate
@@ -70,10 +112,12 @@ static void command_status(IotclEventData data, bool status, const char *command
  * This is started by the initialization code in app_main.c which first performs board and
  * networking initialization
  */
+
 void iotconnect_app( void * pvParameters )
 {
     BaseType_t result = pdFALSE;
-
+    static TickType_t lastBlinkTime = 0;
+    static bool ledState = false;
     result = init_sensors();
 
     if(result != pdTRUE) {
@@ -81,76 +125,87 @@ void iotconnect_app( void * pvParameters )
         vTaskDelete( NULL );
     }
 
-    // Get some settings from non-volatile storage.  These can be set on the command line
-    // using the conf command.
-
     char *device_id = KVStore_getStringHeap(CS_CORE_THING_NAME, NULL);   // Device ID
     char *cpid = KVStore_getStringHeap(CS_IOTC_CPID, NULL);
     char *iotc_env = KVStore_getStringHeap(CS_IOTC_ENV, NULL);
 
     if (device_id == NULL || cpid == NULL || iotc_env == NULL) {
-    	LogError("IOTC configuration, thing_name, cpid or env are not set\n");
-		vTaskDelete(NULL);
+        LogError("IOTC configuration, thing_name, cpid or env are not set\n");
+        vTaskDelete(NULL);
     }
-
-    // IoT-Connect configuration setup
 
     IotConnectClientConfig *config = iotconnect_sdk_init_and_get_config();
     config->cpid = cpid;
-	config->env = iotc_env;
-	config->duid = device_id;
-	config->cmd_cb = on_command;
-	config->ota_cb = NULL;
-	config->status_cb = NULL;
-	config->auth_info.type = IOTC_X509;
-    config->auth_info.https_root_ca              = xPkiObjectFromLabel( TLS_HTTPS_ROOT_CA_CERT_LABEL );
-    config->auth_info.mqtt_root_ca               = xPkiObjectFromLabel( TLS_MQTT_ROOT_CA_CERT_LABEL );
+    config->env = iotc_env;
+    config->duid = device_id;
+    config->cmd_cb = on_command;
+    config->ota_cb = NULL;
+    config->status_cb = NULL;
+    config->auth_info.type = IOTC_X509;
+    config->auth_info.https_root_ca = xPkiObjectFromLabel( TLS_HTTPS_ROOT_CA_CERT_LABEL );
+    config->auth_info.mqtt_root_ca = xPkiObjectFromLabel( TLS_MQTT_ROOT_CA_CERT_LABEL );
     config->auth_info.data.cert_info.device_cert = xPkiObjectFromLabel( TLS_CERT_LABEL );
-    config->auth_info.data.cert_info.device_key  = xPkiObjectFromLabel( TLS_KEY_PRV_LABEL );;
+    config->auth_info.data.cert_info.device_key = xPkiObjectFromLabel( TLS_KEY_PRV_LABEL );
 
 #if defined(IOTCONFIG_USE_DISCOVERY_SYNC)
-    // Get MQTT configuration from discovery and sync
     iotconnect_sdk_init(NULL);
 #else
-    // Not using Discovery and Sync so some additional settings, are obtained from the CLI,
     char *mqtt_endpoint_url = KVStore_getStringHeap(CS_CORE_MQTT_ENDPOINT, NULL);
     char *telemetry_cd = KVStore_getStringHeap(CS_IOTC_TELEMETRY_CD, NULL);
 
     if (mqtt_endpoint_url == NULL || telemetry_cd == NULL) {
-    	LogError ("IOTC configuration, mqtt_endpoint, telemetry_cd not set");
-    	vTaskDelete( NULL );
+        LogError ("IOTC configuration, mqtt_endpoint, telemetry_cd not set");
+        vTaskDelete(NULL);
     }
 
     awsrtos_config.host = mqtt_endpoint_url;
-	awsrtos_config.telemetry_cd = telemetry_cd;
-	iotconnect_sdk_init(&awsrtos_config);
+    awsrtos_config.telemetry_cd = telemetry_cd;
+    iotconnect_sdk_init(&awsrtos_config);
 #endif
 
     while (1) {
-        /* Interpret sensor data */
         int32_t sensor_error = BSP_ERROR_NONE;
         BSP_MOTION_SENSOR_Axes_t xAcceleroAxes, xGyroAxes, xMagnetoAxes;
 
-        sensor_error = BSP_MOTION_SENSOR_GetAxes( 0, MOTION_GYRO, &xGyroAxes );
-        sensor_error |= BSP_MOTION_SENSOR_GetAxes( 0, MOTION_ACCELERO, &xAcceleroAxes );
-        sensor_error |= BSP_MOTION_SENSOR_GetAxes( 1, MOTION_MAGNETO, &xMagnetoAxes );
+        sensor_error = BSP_MOTION_SENSOR_GetAxes(0, MOTION_GYRO, &xGyroAxes);
+        sensor_error |= BSP_MOTION_SENSOR_GetAxes(0, MOTION_ACCELERO, &xAcceleroAxes);
+        sensor_error |= BSP_MOTION_SENSOR_GetAxes(1, MOTION_MAGNETO, &xMagnetoAxes);
 
         if (sensor_error == BSP_ERROR_NONE) {
+            float simulatedTempF = simulateTemperatureRise();  // Get simulated temperature
+
             IotclMessageHandle message = iotcl_telemetry_create();
-            char* json_message = create_telemetry_json(message, xAcceleroAxes, xGyroAxes, xMagnetoAxes);
+            char* json_message = create_telemetry_json(message, xAcceleroAxes, xGyroAxes, xMagnetoAxes, simulatedTempF);
 
             if (json_message == NULL) {
-            	LogError("Could not create telemetry data\n");
-                vTaskDelete( NULL );
+                LogError("Could not create telemetry data\n");
+                vTaskDelete(NULL);
             }
 
-			iotconnect_sdk_send_packet(json_message);  // underlying code will report an error
-			iotcl_destroy_serialized(json_message);
+            iotconnect_sdk_send_packet(json_message);  // Send telemetry data
+            iotcl_destroy_serialized(json_message);
         }
 
-        vTaskDelay( pdMS_TO_TICKS( MQTT_PUBLISH_PERIOD_MS ) );
+        TickType_t currentTime = xTaskGetTickCount();
+
+        if (!targetTemperatureReached) {
+            if (currentTime - lastBlinkTime >= pdMS_TO_TICKS(LED_BLINK_PERIOD_MS)) {
+                lastBlinkTime = currentTime; // Correctly update the last blink time
+                ledState = !ledState;        // Toggle the LED state
+                if (ledState) {
+                    BSP_LED_On(LED_RED);
+                } else {
+                    BSP_LED_Off(LED_RED);
+                }
+            }
+        } else {
+            BSP_LED_On(LED_RED); // Keep the LED on when the target temperature is reached
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(MQTT_PUBLISH_PERIOD_MS));
     }
 }
+
 
 
 /* @brief	Initialize the dev board's accelerometer, gyro and magnetometer sensors
@@ -179,7 +234,7 @@ static BaseType_t init_sensors( void )
  *
  */
 static char *create_telemetry_json(IotclMessageHandle msg, BSP_MOTION_SENSOR_Axes_t accel_data,
-								BSP_MOTION_SENSOR_Axes_t gyro_data, BSP_MOTION_SENSOR_Axes_t mag_data) {
+								BSP_MOTION_SENSOR_Axes_t gyro_data, BSP_MOTION_SENSOR_Axes_t mag_data, float simulatedTempF) {
 
     // Optional. The first time you create a data point, the current timestamp will be automatically added
     // TelemetryAddWith* calls are only required if sending multiple data points in one packet.
@@ -200,6 +255,9 @@ static char *create_telemetry_json(IotclMessageHandle msg, BSP_MOTION_SENSOR_Axe
 #endif
 
     iotcl_telemetry_set_string(msg, "version", APP_VERSION);
+
+    // Add the simulated temperature to the telemetry data
+    iotcl_telemetry_set_number(msg, "simulated_temp", simulatedTempF);
 
     const char* str = iotcl_create_serialized_string(msg, false);
 
@@ -241,6 +299,24 @@ static void on_command(IotclEventData data) {
 				BSP_LED_Off(LED_GREEN);
 			}
 			command_status(data, true, command, "OK");
+		} else if (NULL != strstr(command, "set-temp")) {
+			// Parse the temperature value from the command and set it to presetTempF
+			// Assuming the command format is "set-temp value"
+			float tempVal;
+			if (sscanf(command, "set-temp %f", &tempVal) == 1) {
+				presetTempF = tempVal;
+				simulateTemperature = true;  // Start simulation
+				simStartTime = xTaskGetTickCount();
+				LogInfo("Temperature set to %.2f", presetTempF);
+				command_status(data, true, command, "Temperature set");
+			} else {
+				command_status(data, false, command, "Invalid temperature value");
+			}
+//		} else if(NULL != strstr(command, "start-temp-sim")) {
+//	        simulateTemperature = true;
+//	        simStartTime = xTaskGetTickCount();
+//	        LogInfo("Starting temperature simulation");
+//	        command_status(data, true, command, "Temperature simulation started");
 		} else {
 			LogInfo("command not recognized");
 			command_status(data, false, command, "Not implemented");
@@ -270,3 +346,25 @@ static void command_status(IotclEventData data, bool status, const char *command
 	iotconnect_sdk_send_packet(ack);
 	free((void*) ack);
 }
+
+/*static float simulateTemperatureRise() {
+    if (!simulateTemperature) {
+        return startTempF;
+    }
+
+    TickType_t currentTime = xTaskGetTickCount();
+    float elapsedSec = (float)(currentTime - simStartTime) / pdMS_TO_TICKS(1000);
+
+    // Check if the elapsed time is less than the simulation duration
+    if (elapsedSec < simDurationSec) {
+        // Simulate the temperature rise
+        float tempRise = (presetTempF - startTempF) * (elapsedSec / simDurationSec);
+        return startTempF + tempRise;
+    } else {
+        // Keep the temperature at the preset value
+        return presetTempF;
+    }
+}
+*/
+
+
